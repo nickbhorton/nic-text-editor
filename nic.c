ac2d710bbfc6ce927a1de9b81c6ba5286917b3d5
Nick Horton <nickkbhorton@gmail.com>
2024-02-21 02:59:23 -0600

    begining of string implementation

--- nic.c
+++ nic.c
@@ -1,7 +1,36 @@
-#include "unistd.h"
-#include "string.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char* FG_RED    = "\x1b[38;5;9m";
+const char* FG_GREEN  = "\x1b[38;5;10m";
+const char* FG_BLUE   = "\x1b[38;5;12m";
+
+const char* CLEAR     = "\x1b[m";
+
+typedef struct string {
+    char* chars;
+    size_t len;
+    size_t cap;
+} String;
+
+String* string_new() {
+    String* result = malloc(sizeof(String));
+    result->chars = malloc(16);
+    result->cap = 16;
+    result->len = 0;
+    memset(result->chars, 0, result->cap);
+    return result;
+}
+
+void string_append(String* str_ptr, const char* string_literal) {
+    size_t length_string_literal = strlen(string_literal);
+    /// TODO
+}
+
 int main () {
-    const char* str = "\x1b[48;5;21m testing \x1b[m\n";
-    write(STDOUT_FILENO, str, strlen(str));
+    String* ptr = string_new();
+    string_append(ptr, "hello world");
+    write(STDOUT_FILENO, ptr, ptr->len);
     return 0;
 }
